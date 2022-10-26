8fb360cda5adefb9ec177f742741c5757d9d69b9
Richard Mansfield <richard@dragonfly.co.nz>
2022-10-26 16:53:24 +1300

    Copy example aebr to output dir

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ make
 cp mpi-latex-templates*.deb /output/
 cp latex/mpi.pdf /output/
 cp examples/mpi-far.pdf /output/
+cp examples/mpi-aebr.pdf /output/
 cp examples/mpi-plenary.pdf /output/
 #cp examples/*.bib /output/
