af4610db1b7eefacbbffbf07292058432d60234d
David Middleton <david@pisces.nz>
2024-01-28 12:32:24 +1300

    examine tex

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ make
 cp mpi-latex-templates*.deb /output/
 cp latex/mpi.pdf /output/
 cp examples/*.pdf /output/.
+cp examples/mpi-aebr.tex /output/.
 #cp examples/*.bib /output/
 #cp examples/mpi-tables.tex /output/.
