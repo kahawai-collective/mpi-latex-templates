e1a80b5a0c45ef28e00885068b6ab07733a19d74
David Middleton <david@pisces.nz>
2024-11-30 17:27:26 +1300

    postinst

--- build.sh
+++ build.sh
@@ -3,9 +3,11 @@ set -ex
 export RUN=
 export TEXINPUTS=.///:..//latex//:..//graphics//:
 
+./package/debian/mpi-latex-templates.postinst
+
 make
 cp mpi-latex-templates*.deb /output/
-#cp latex/mpi.pdf /output/
-#cp examples/*.pdf /output/.
+cp latex/mpi.pdf /output/
+cp examples/*.pdf /output/.
 #cp examples/*.tex /output/
 #cp examples/mpi-tables.tex /output/.
