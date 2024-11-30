8b13be7b5ca615cdb5e7146f4a76bf4666de2419
David Middleton <david@pisces.nz>
2024-11-30 16:18:59 +1300

    examine apa

--- build.sh
+++ build.sh
@@ -3,9 +3,11 @@ set -ex
 export RUN=
 export TEXINPUTS=.///:..//latex//:..//graphics//:
 
-make
-cp mpi-latex-templates*.deb /output/
-cp latex/mpi.pdf /output/
-cp examples/*.pdf /output/.
-#cp examples/*.tex /output/
-#cp examples/mpi-tables.tex /output/.
+cp -r /usr/share/texlive/texmf-dist/tex/latex/biblatex-apa /output
+
+# make
+# cp mpi-latex-templates*.deb /output/
+# cp latex/mpi.pdf /output/
+# cp examples/*.pdf /output/.
+# #cp examples/*.tex /output/
+# #cp examples/mpi-tables.tex /output/.
