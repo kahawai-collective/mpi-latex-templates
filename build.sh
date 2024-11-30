87ef2460cc7b5a6222a354ca85f436c8d6700336
David Middleton <david@pisces.nz>
2024-11-30 16:14:55 +1300

    reinstate biber patches

--- build.sh
+++ build.sh
@@ -3,11 +3,9 @@ set -ex
 export RUN=
 export TEXINPUTS=.///:..//latex//:..//graphics//:
 
-/usr/bin/biber --version
-
-# make
-# cp mpi-latex-templates*.deb /output/
-# cp latex/mpi.pdf /output/
-# cp examples/*.pdf /output/.
-# #cp examples/*.tex /output/
-# #cp examples/mpi-tables.tex /output/.
+make
+cp mpi-latex-templates*.deb /output/
+cp latex/mpi.pdf /output/
+cp examples/*.pdf /output/.
+#cp examples/*.tex /output/
+#cp examples/mpi-tables.tex /output/.
