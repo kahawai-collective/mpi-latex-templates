98a16dc6bfb743cf1b52d94158c154819aed2eee
David Middleton <david@pisces.nz>
2024-11-30 18:57:15 +1300

    plenary example

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ bash ./package/debian/mpi-latex-templates.postinst
 make
 cp mpi-latex-templates*.deb /output/
 cp latex/mpi.pdf /output/
-#cp examples/*.pdf /output/.
+cp examples/*.pdf /output/.
 #cp examples/*.tex /output/
 #cp examples/mpi-tables.tex /output/.
