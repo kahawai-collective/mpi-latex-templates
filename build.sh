810bf8dbb95db0243ed77789d4b9ba706f55451e
David Middleton <david@pisces.nz>
2024-11-30 16:09:51 +1300

    check biber version

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 set -ex
 export RUN=
 export TEXINPUTS=.///:..//latex//:..//graphics//:
-make
-cp mpi-latex-templates*.deb /output/
-cp latex/mpi.pdf /output/
-cp examples/*.pdf /output/.
-#cp examples/*.tex /output/
-#cp examples/mpi-tables.tex /output/.
+
+/usr/bin/biber --version
+
+# make
+# cp mpi-latex-templates*.deb /output/
+# cp latex/mpi.pdf /output/
+# cp examples/*.pdf /output/.
+# #cp examples/*.tex /output/
+# #cp examples/mpi-tables.tex /output/.
